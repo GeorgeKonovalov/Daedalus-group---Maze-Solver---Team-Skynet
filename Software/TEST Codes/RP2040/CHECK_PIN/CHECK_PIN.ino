c953163bb4ef5c466d6d408efdacd2fd1708321f
GeorgeKonovalov <georgijcon@yandex.ru>
2026-04-05 16:36:28 +0100

    TEST Voltage on RP2040
    Wasn't upload on my PC

---
+++ Software/TEST Codes/RP2040/CHECK_PIN/CHECK_PIN.ino
@@ -0,0 +1,14 @@
+const int Pin_Check = 13;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(Pin_Check, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(Pin_Check, HIGH); 
+  // put your main code here, to run repeatedly:
+
+}
